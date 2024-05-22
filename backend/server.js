32a7d786dde068386c24ee060275f40975378d74
Pernilla Sterner <psterner@hotmail.com>
2024-05-22 14:44:23 +0200

    Big chunk of code including register user, login user and components for button, play and myprogress

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt-nodejs";
 import crypto from "crypto";
 
+// Defining port and connecting to mongoose
+const port = process.env.PORT || 8000;
+const app = express();
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth-users";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
@@ -19,29 +22,37 @@ const User = mongoose.model("User", {
     type: String,
     required: true,
   },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString("hex"),
   },
 });
 
-// Defining port
-const port = process.env.PORT || 8000;
-const app = express();
-
+//Authenticate user as middleware
 const authenticateUser = async (req, res, next) => {
-  // Using authorization from header to find user
   const user = await User.findOne({ accessToken: req.header("Authorization") });
-
-  // Checking if user is found
   if (user) {
     console.log("User is found", user);
-    // Modifing request to add the user to the request
     req.user = user;
-    // Allowing express to continue with the api request
     next();
   } else {
-    // User is not authorized to continue with the request
     req.status(401).json({ loggedOut: true });
   }
 };
@@ -52,47 +63,59 @@ app.use(express.json());
 
 // Defining routes
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello friend!");
 });
-
-app.post("/tweets", authenticateUser);
-// app.post("/tweets", async (req, res) => {
-//   // This will only happen if the next() function is called from the middleware
-//   // now we can access the req.user object from the middleware
-// });
 
 //Create user with username and password
 app.post("/users", async (req, res) => {
   try {
+    const { username, firstName, lastName, age, email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const user = new User({
-      username: req.body.username,
-      password: bcrypt.hashSync(req.body.password, salt),
+      username,
+      firstName,
+      lastName,
+      age,
+      email,
+      password: bcrypt.hashSync(password, salt),
     });
-
-    console.log("Trying to create new user" + user);
     await user.save();
-    res.status(201).json({ id: user._id, accessToken: user.accessToken });
-  } catch (err) {
-    console.error("Error creating user:", err);
+    res.status(201).json(user);
+    //res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (error) {
+    console.error("Error creating user:", error);
     res
       .status(400)
-      .json({ message: "Could not create user.", errors: err.errors });
+      .json({ response: error, message: "Could not create user." });
   }
 });
 
 //Endpoint for login
 app.post("/sessions", async (req, res) => {
-  const user = await User.findOne({ username: req.body.username });
+  const userByUsername = await User.findOne({ username: req.body.username });
+  const userByEmail = await User.findOne({ email: req.body.email });
 
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken });
+  if (
+    userByUsername &&
+    bcrypt.compareSync(req.body.password, userByUsername.password)
+  ) {
+    res.json({
+      userId: userByUsername._id,
+      accessToken: userByUsername.accessToken,
+    });
+  } else if (
+    userByEmail &&
+    bcrypt.compareSync(req.body.password, userByEmail.password)
+  ) {
+    res.json({ userId: userByEmail._id, accessToken: userByEmail.accessToken });
   } else {
-    // Failure
-    // a. User doesn't exist
-    // b. Encrypted password doesn't match
     res.json({ notFound: true });
   }
+});
+
+app.post("/games", authenticateUser);
+app.post("/games", async (req, res) => {
+  res.send("You are logged in");
 });
 
 // Start the server
