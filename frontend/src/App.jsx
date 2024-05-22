72dd86bbb60cda5940198013f55868f1d531b3dc
Pernilla Sterner <psterner@hotmail.com>
2024-05-22 11:54:26 +0200

    Import header, login and registration components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Header } from "./components/Header";
 import { RegistrationForm } from "./components/RegistrationForm";
+import { Login } from "./components/Login";
+import { UserProvider } from "./contexts/UserContext";
 
 export const App = () => {
   return (
-    <main>
-      <RegistrationForm />
-    </main>
+    <UserProvider>
+      <Header />
+      <main>
+        <RegistrationForm />
+        <Login />
+      </main>
+    </UserProvider>
   );
 };
