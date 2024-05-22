8fb20c3a94b420a0f7f36cd11a3b121257ea667c
Pernilla Sterner <psterner@hotmail.com>
2024-05-22 07:27:16 +0200

    Create route for testing password

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,46 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    // Random string of bytes
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+/* Example
+// POST request
+const request = { name: "Bob", password: "foobar" };
+
+// DB Entry
+const dbEntry = { name: "Bob", password: "45lkjt5elk52" };
+
+bcrypt.compareSync(request.password, dbEntry.password);
+*/
+
+const user = new User({ name: "Bob", password: bcrypt.hashSync("foobar") });
+user.save();
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -19,6 +50,20 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.post("/sessions", async (req, res) => {
+  const user = await User.findOne({ name: req.body.name });
+
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    // Success
+    res.json({ userId: user._id, accessToken: user.accessToken });
+  } else {
+    // Failure
+    // a. User doesn't exist
+    // b. Encrypted password doesn't match
+    res.json({ notFound: true });
+  }
 });
 
 // Start the server
